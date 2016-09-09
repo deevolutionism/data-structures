5c5277f09ccdf7b4b735b75535419a967c13a232
Gentry Demchak <deevolutionism@gmail.com>
2016-09-09 18:50:48 -0400

    segets info for first element

--- assignment-2/server.js
+++ assignment-2/server.js
@@ -20,18 +20,44 @@ var htmlScraper = (function(){
   var files = [];
 
   var readFileRecursively = function(path,count,suffix){
-    if(count < 10){
-      count++;
-      count = '0' + count;
+    var new_count = ++count;
+    if(new_count < 10){
+      new_count = '0' + new_count;
     }
-    console.log('reading ' + path + count + suffix);
-    fs.readFile(path+count+suffix,'utf8',function(err,data){
+    console.log('reading ' + path + new_count + suffix + ' >>>>>>>>>>>>>>');
+    fs.readFile(path+new_count+suffix,'utf8',function(err,data){
       if(err){
         console.log(err);
       } else {
         var $ = cheerio.load(data);
-        console.log(data);
+        // var indexed = $('center').get();
+        var indexed = $('table')
+        .children('tbody')
+        .each(function(i,elem){
+          $('.detailsBox').remove();
+          var location = $(elem).children('tr').find('td').eq(0);
+          // location.remove('h4');
+          console.log(location.text());
+          // console.log($(elem).children('tr').find('td').eq(0).text());
+        });
+        // console.log(indexed);
+        // .each(function(i,elem){
+        //   // console.log($(this).find('h4').text());
+        //   console.log('--------------')
+        //   console.log($(elem).length);
+        //   // $(elem).remove('h4');
+        //   // $(elem).remove('br');
+        //   // $(elem).remove('b');
+        //   // $(elem).remove('div');
+        //   // console.log($(elem).text());
+        // });
+
+        // console.log(indexed);
+        // var sliced = $('center').slice(1).eq(0).text();
+        // console.log(indexed);
+        if(count < 1){
         readFileRecursively(path,count,suffix);
+        }
       }
     });
   };
