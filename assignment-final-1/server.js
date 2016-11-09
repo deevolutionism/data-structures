d4d26b15427591cc320c9626d722148be90e830e
Gentry Demchak <deevolutionism@gmail.com>
2016-11-09 15:11:19 -0500

    aggregates based on current time and day

--- assignment-final-1/server.js
+++ assignment-final-1/server.js
@@ -11,13 +11,13 @@ app.use(express.static('public'));
 
 app.get('/latlng', (req, res) => {
   console.log('processing request . . . ');
-  query2( (data) => {
+  aggregate( (data) => {
     res.send(data)
   });
-  // res.send(query2());
 });
 
 /*
+EXAMPLE OUTPUT JSON 
 {
   'location': 'utica ave, brooklyn ny, 11233',
   'geometries': {
@@ -34,108 +34,48 @@ app.get('/latlng', (req, res) => {
 }
 */
 
-
+/*
+DB AGGREGATE QUERY
+db.aagroups_02.aggregate([ {$match: {$and:[{day:"0"},{time:"08:00"}]} } ])
+*/
 
 var aggregate = (callback) => {
   var datetimeStart = new Date();
-  var hour = datetimeStart.getHours();
-  console.log(datetimeStart.getDay());
-  hour = hour + ':00';
-  console.log(hour);
+  var d = datetimeStart.getDay();
+  var h = datetimeStart.getHours();
+  console.log(`day:${d} + hour:${h}`);
   MongoClient.connect(url, (err, db) => {
-    if(err){return console.dir(err)}
+    if (err) {return console.dir(err);}
+
     var collection = db.collection('aagroups_02');
+
     collection.aggregate([
-      { $match : { time: { $gte: hour }, day: toString(datetimeStart.getDay()) } },
-    ]).toArray((err, docs) => {
+      {
+        $match: {
+          $and: [
+            {day:d.toString()},
+            {time: {$gte:getHour(h)} }
+          ]
+        }
+      }
+    ]).toArray( (err, docs) => {
       if(err) {console.log(err)}
       else {
         console.log(docs);
-        var locations = {}
-        docs.forEach((meetinggroup)=>{
-          //loop through each location to check if it already exists
-          for(var j = 0; j<locations.length;j++){
-
-            //the meeting group already exists at this location, so
-            //first, check if the meeting time for that group has been added.
-            if(meetinggroup.formatted_address == locations[j].address){
-
-              //
-              if(locations[j].groups.name !== meetinggroup.name){
-                locations[j].groups.push({
-                  'name':location.name,
-                  'type':location.type,
-                  'time':[location.time]
-                });
-              } else {
-
-              }
-
-            } else {
-              //location doesn't exist yet, add it.
-              locations.push({
-                'address':location.formatted_address,
-                'geometries':{'lat':location.lat,'lng':location.lng},
-                'groups':[
-                  {
-                    'name':location.name,
-                    'type':location.type,
-                    'time':[location.time]
-                  }
-                ]
-              });
-            }
-          }
-        });
+        callback({'result':docs});
       }
     });
-  });
-}
 
-// aggregate();
-
-var query = (callback) => {
-  var datetimeStart = new Date();
-  MongoClient.connect(url, (err, db) => {
-    if (err) {return console.dir(err);}
-
-    var collection = db.collection('aagroups_02');
-
-    collection.find({
-      $and: [
-        {'day':datetimeStart.getDay()},
-        {'time':{$gte:datetimeStart.getHours()}}
-      ]
-    }).toArray( (err, docs) => {
-        if (err) {console.log(err)}
-        else {
-          // console.log(docs.length);
-          callback({'result':docs});
-        }
-        db.close();
-        // noIndex.push(new Date() - datetimeStart);
-        // console.log("This process completed in", new Date() - datetimeStart, "milliseconds.");
-        // console.log(noIndex);
-    });
   });
 }
 
 
-var query2 = (callback) => {
-  var datetimeStart = new Date();
-  MongoClient.connect(url, (err, db) => {
-    if (err) {return console.dir(err);}
-
-    var collection = db.collection('aagroups_02');
-
-    collection.find({})
-    .toArray( (err, docs) => {
-        if (err) {console.log(err)}
-        else {
-          console.log(docs);
-          callback({'result':docs});
-        }
-        db.close();
-    });
-  });
+var getHour = (hour) => {
+  if(hour < 10){
+    var h = `0${hour}:00`;
+    return h
+  } else {
+    var h = `${hour}:00`;
+    return h
+  }
 }
