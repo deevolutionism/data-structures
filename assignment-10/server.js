6f1b52cffcf0b6dbd31253f2336e991f0dfd7dc7
Gentry Demchak <deevolutionism@gmail.com>
2016-11-29 11:30:57 -0500

    assignment 10 added

---
+++ assignment-10/server.js
@@ -0,0 +1,37 @@
+var pg = require('pg');
+var http = require('http');
+
+// connection string
+var un = 'litterbox';
+var pw = 'litterbox';
+var db = 'litterbox';
+var ep = 'litterbox.cd9twrbg1o5s.us-west-2.rds.amazonaws.com:5432';
+var conString = `postgres://${un}:${pw}@${ep}/${db}`;+ un + ":" + pw + "@" + ep + "/" + db;
+
+var query = "SELECT * FROM litterbox;";
+
+var server = http.createServer(function(req, res) {
+
+    pg.connect(conString, function(err, client, done) {
+        if (err) {
+            return console.error('error fetching client from pool', err);
+        }
+
+        client.query(query, function(err, result) {
+            //call `done()` to release the client back to the pool
+            done();
+
+            if (err) {
+                return console.error('error running query', err);
+            }
+
+            res.writeHead(200, {'content-type': 'application/json'});
+            res.end(JSON.stringify(result.rows));
+
+        }); // client.query
+
+    }); // pg.connect
+
+}); // server
+
+server.listen(8000);
